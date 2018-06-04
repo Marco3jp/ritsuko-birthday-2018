e4c24c09e0f0613735dd6557f55997e7f9db26f0
marco <28888207+Marco3jp@users.noreply.github.com>
2018-06-04 21:35:05 +0900

    クラス名の変更に基づいて修正した

--- src/www/js/module/keepAspectRatio.js
+++ src/www/js/module/keepAspectRatio.js
@@ -1,6 +1,6 @@
 class KeepAspectRatio {
     constructor(){ //DOM生成後に動く必要ありっぽい
-        this.wrapper = document.querySelector(".wrapper");
+        this.wrapper = document.querySelector(".landscape");
         this.state = 100;
         this.changeWindowSize();
     }
@@ -9,15 +9,15 @@ class KeepAspectRatio {
         if( (window.innerWidth/window.innerHeight) > (16/9) ){ //縦合わせ時
             console.log("yokonaga");
             if(this.state!==1){
-                this.wrapper.classList.add("wrapper-horizontal");
-                this.wrapper.classList.remove("wrapper-vertical");
+                this.wrapper.classList.add("landscape-horizontal");
+                this.wrapper.classList.remove("landscape-vertical");
                 this.state=1;
             }
         }else{ //横合わせ時
             console.log("tatenaga");
             if(this.state!==0){
-                this.wrapper.classList.add("wrapper-vertical");
-                this.wrapper.classList.remove("wrapper-horizontal");
+                this.wrapper.classList.add("landscape-vertical");
+                this.wrapper.classList.remove("landscape-horizontal");
                 this.state=0
             }
         }
