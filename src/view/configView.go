c5606323fa664131d30ff79daf849d5e8c7ca4e0
marco <28888207+Marco3jp@users.noreply.github.com>
2018-06-07 01:40:44 +0900

    ヘッダーをセットし、ACAOを*にしてレスポンスを返す部分を書いた

--- src/view/configView.go
+++ src/view/configView.go
@@ -1,9 +1,14 @@
 package view
 
 import (
-	"../model"
+	"net/http"
+	"fmt"
 )
 
-func GetConfig()  {
-	var config model.Config
+func GetConfig(configJson *[]byte,w http.ResponseWriter)  {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprint(w,string(*configJson))
 }
--- src/view/sceneView.go
+++ src/view/sceneView.go
@@ -1,10 +1,14 @@
 package view
 
 import (
-	"../model"
+	"net/http"
+	"fmt"
 )
 
-func GetScene()  {
-	var scene model.Scene
+func GetScene(sceneJson *[]byte,w http.ResponseWriter)  {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
 
+	fmt.Fprint(w,string(*sceneJson))
 }
