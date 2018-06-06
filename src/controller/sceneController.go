941c2310a77741f87d83f2d38ec7523c22b9870a
marco <28888207+Marco3jp@users.noreply.github.com>
2018-06-07 01:36:01 +0900

    シーンを取得し、レスポンスを返す部分を書いた

--- src/controller/sceneController.go
+++ src/controller/sceneController.go
@@ -1,11 +1,29 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"../module"
+	"strconv"
+	"../view"
+	"log"
+)
 
 type SceneController struct {
 	*BaseController
 }
 
 func (self SceneController) GetScene(w http.ResponseWriter, r *http.Request)  {
+	sceneId,err := strconv.Atoi(r.URL.Query().Get("id"))
 
+	if err!=nil{
+		panic(err)
+	}
+
+	sceneJson,err := module.SceneModule{self.DB}.GetScene(&sceneId)
+
+	if err != nil{
+		log.Printf("%+v",err)
+	}
+
+	view.GetScene(sceneJson,w)
 }
