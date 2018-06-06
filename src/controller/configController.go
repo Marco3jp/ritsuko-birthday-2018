b4d0ba218af5754dcadebc7ce12bf27681c8c71f
marco <28888207+Marco3jp@users.noreply.github.com>
2018-06-07 01:30:13 +0900

    コンフィグの取得とレスポンスを行う部分を書いた

--- src/controller/configController.go
+++ src/controller/configController.go
@@ -1,13 +1,35 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"../module"
+	"../view"
+)
 
 type ConfigController struct {
 	*BaseController
 }
 
 func (self ConfigController) GetConfig(w http.ResponseWriter, r *http.Request)  {
-	
+	userId, err := strconv.Atoi(r.URL.Query().Get("userId"))
+
+	if err!=nil{
+		panic(err)
+	}
+
+	/*
+	token, err := strconv.Atoi(r.URL.Query().Get("token"))
+	if err!=nil{
+		panic(err)
+	}
+	*/
+
+	token := "6eba0f8b-51eb-47bd-b1c3-ab60d56666e2"
+	configJson := module.ConfigModule{self.DB}.GetConfig(userId,token)
+
+	view.GetConfig(configJson,w)
+
 }
 
 /*
