b332dc88c58b25a57a4cc6f88b7dd7c98b59f53b
marco <28888207+Marco3jp@users.noreply.github.com>
2018-06-13 23:40:12 +0900

    "情報"の類をStateから分離した。

---
+++ src/www/js/model/info.js
@@ -0,0 +1,11 @@
+class Info {
+    constructor(){
+        this.idolRealName="";
+        this.idolName="";
+        this.producerName="";
+        this.scene={};
+        this.timeoutID=0;
+    }
+}
+
+let info = new Info;
